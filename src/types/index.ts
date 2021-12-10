a290d9171ff45473b3aa87f31825e810a4b5f196
DucQuocXa <duc.ngogia310302@gmail.com>
2021-12-10 22:21:33 +0200

    add readme image

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "http://localhost:5000/api/v1";
+export const baseURL = "https://beau-e-commerce.herokuapp.com/api/v1/";
 
 export const API = axios.create({
   baseURL: `${baseURL}`,
